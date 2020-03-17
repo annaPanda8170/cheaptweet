6f7b6babb90c6c4bf6325f668e2d71cd04affe83
annaPanda8170 <handa_ryohei@yahoo.co.jp>
2020-03-18 05:35:15 +0900

    ⑥詳細(show)・編集(edit→update)・削除(destroy)⑦編集・削除の権限設定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  before_action :unless_signin, only: [:new, :create,]
+  before_action :unless_mytweet, only: [:edit, :update, :destroy]
   def index
     @tweets = Tweet.all.includes(:user).order("created_at DESC")
   end
@@ -13,8 +15,33 @@ class TweetsController < ApplicationController
       render action: :new
     end
   end
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+  def update
+    @tweet = Tweet.find(params[:id])
+    if @tweet.update(tweet_params)
+      redirect_to tweet_path(params[:id])
+    else
+      render action: :edit
+    end
+  end
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.delete
+    # redirect_to root_path
+  end
   private
   def tweet_params
     params.require(:tweet).permit(:text).merge(user_id: current_user.id)
+  end
+  def unless_signin
+    redirect_to tweets_path unless user_signed_in?
+  end
+  def unless_mytweet
+    redirect_to tweets_path unless user_signed_in? && current_user.id == Tweet.find(params[:id]).user.id
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :tweets, only: [:index, :new, :create]
+  resources :tweets
   devise_for :users
   root 'tweets#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

ef0bf9d21dac3a628f34a1dfdbb16b2735f34534
annaPanda8170 <handa_ryohei@yahoo.co.jp>
2020-03-18 04:36:27 +0900

    ④投稿(new→create)

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,19 @@
 class TweetsController < ApplicationController
   def index
   end
+  def new
+    @tweet = Tweet.new
+  end
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+  private
+  def tweet_params
+    params.require(:tweet).permit(:text).merge(user_id: current_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :tweets, only: [:index]
+  resources :tweets, only: [:index, :new, :create]
   devise_for :users
   root 'tweets#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

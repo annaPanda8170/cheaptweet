42641429d1ee5a9291984d7b21ccf21a14fb9521
annaPanda8170 <handa_ryohei@yahoo.co.jp>
2020-03-18 23:09:29 +0900

    ルーティング不具合解消

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   resources :tweets do
     resources :comments, only: :create
   end
-  resources :users, only: :show
   devise_for :users
+  resources :users, only: :show
   root 'tweets#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

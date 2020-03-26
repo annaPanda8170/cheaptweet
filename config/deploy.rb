3470608a0d75100363e25509eba9defde15e3b6d
annaPanda8170 <handa_ryohei@yahoo.co.jp>
2020-03-26 22:46:38 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,16 @@
-lock (3.12.1)
+lock '3.12.1'
 
-set :application, cheaptweet
+set :application, 'cheaptweet'
 
 set :repo_url,  'git@github.com:annaPanda8170/cheaptweet.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby,  2.5.1
+set :rbenv_ruby,  '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: [~/.ssh/cheaptweet.pem] 
+                  keys: ['~/.ssh/cheaptweet.pem'] 
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 

c5315c45f46ee6b85035cc2b48a8504a98717121
annaPanda8170 <handa_ryohei@yahoo.co.jp>
2020-03-27 01:07:30 +0900

    デプロイ失敗対策（実験7）
    config/deploy.rbに追記

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,10 +6,17 @@ set :repo_url,  'git@github.com:annaPanda8170/cheaptweet.git'
 
 set :branch, 'ツイッター風Railsアプリをデプロイする'
 
+set :deploy_to, '/var/www/cheaptweet'
+
+set :linked_files, fetch(:linked_files, []).push('config/settings.yml')
+
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
+
 set :rbenv_ruby,  '2.5.1'
+
+set :log_level, :debug
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/cheaptweet.pem'] 
